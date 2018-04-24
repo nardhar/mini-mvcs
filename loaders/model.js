0b4e9d8f9667eff6d32f1c1fe63251744e254fd2
nardhar <nardhar.mure@gmail.com>
2018-04-24 19:23:42 -0400

    migrating file structure from older projects

---
+++ loaders/model.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const Sequelize = require('sequelize');
+const config = require('./config');
+const fileUtil = require('../util/file');
+
+const db = {};
+
+const sequelize = config.database.use_env_variable
+  ? new Sequelize(process.env[config.database.use_env_variable], config.database.database)
+  : new Sequelize(config.database.database, config.database.username, config.database.password, config.database);
+
+fileUtil.loaddirSync(
+  config.model.dir || '../../../models',
+  config.model.suffix || '.js',
+  config.model.ignore || [],
+  (err, file, filePath) => {
+    const model = sequelize['import'](filePath);
+    db[model.name] = model;
+  },
+);
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
