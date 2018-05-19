1b732121c3684c25536f1ec855a29f4d9f1a5207
nardhar <nardhar.mure@gmail.com>
2018-05-18 21:03:37 -0400

    carga de configuracion para enviar param FORCE y sincronizar la bd

--- loaders/config.js
+++ loaders/config.js
@@ -4,5 +4,9 @@ const env = process.env.NODE_ENV || 'development';
 
 module.exports = (appPath) => {
   const config = require(path.resolve((appPath), 'config'))[env];
-  return Object.assign({}, config, { appPath });
+  const configDatabase = Object.assign({}, config.database || {}, {
+    sync: { force: process.env.FORCE || config.database.sync.force },
+  });
+
+  return Object.assign({}, config, { appPath, database: configDatabase });
 };
