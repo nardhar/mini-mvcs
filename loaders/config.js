685bb933444aa7f1a08dd175cf8083b5346db85a
nardhar <nardhar.mure@gmail.com>
2018-05-21 15:29:18 -0400

    unit test for loaders/config.js

--- loaders/config.js
+++ loaders/config.js
@@ -3,10 +3,13 @@ const path = require('path');
 const env = process.env.NODE_ENV || 'development';
 
 module.exports = (appPath) => {
-  const config = require(path.resolve((appPath), 'config'))[env];
-  const configDatabase = Object.assign({}, config.database || {}, {
-    sync: { force: process.env.FORCE || config.database.sync.force },
+  const config = require(path.resolve((appPath), 'config'))[env] || {};
+  const configDatabase = config.database || {};
+  const configDatabaseSync = configDatabase.sync || {};
+
+  const database = Object.assign({}, configDatabase, {
+    sync: { force: process.env.FORCE || configDatabaseSync.force },
   });
 
-  return Object.assign({}, config, { appPath, database: configDatabase });
+  return Object.assign({}, config, { appPath, database });
 };
---
+++ test/unit/loaders/config.spec.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const fs = require('fs');
+const { expect } = require('chai');
+
+const config = require('../../../loaders/config');
+
+describe('config Loader', () => {
+  describe('load a config file', () => {
+    it('should load a basic config file', (done) => {
+      const configFile = config(path.resolve(__dirname, './configSample'));
+      expect(configFile).to.be.an('object');
+      expect(configFile).to.have.property('appPath');
+      expect(configFile).to.have.property('database');
+      done();
+    });
+  });
+});
