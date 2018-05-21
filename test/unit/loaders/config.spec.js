4523936df253a5c3e92de6159cdfe850ab6b0c23
nardhar <nardhar.mure@gmail.com>
2018-05-21 15:36:26 -0400

    changed folder for reuse in loaders unit tests

--- test/unit/loaders/config.spec.js
+++ test/unit/loaders/config.spec.js
@@ -7,7 +7,7 @@ const config = require('../../../loaders/config');
 describe('config Loader', () => {
   describe('load a config file', () => {
     it('should load a basic config file', (done) => {
-      const configFile = config(path.resolve(__dirname, './configSample'));
+      const configFile = config(path.resolve(__dirname, './srcSample'));
       expect(configFile).to.be.an('object');
       expect(configFile).to.have.property('appPath');
       expect(configFile).to.have.property('database');
