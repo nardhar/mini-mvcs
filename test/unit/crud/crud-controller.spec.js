c6c0d5154584fc5b87206ff5476141000bcdc275
nardhar <nardhar.mure@gmail.com>
2018-08-16 17:29:58 -0400

    crud-controller testing

--- test/unit/crud/crud-controller.spec.js
+++ test/unit/crud/crud-controller.spec.js
@@ -34,87 +34,161 @@ const bookServiceMock = {
 describe('CRUD Controller', () => {
   const crudController = crudControllerDefinition(withTransactionMock);
 
-  describe('Creating a default crud controller', () => {
+  describe('Generation of default methods', () => {
     let app;
     let router;
     let requester;
 
     before(() => {
       app = express();
-      router = express.Router();
       app.use(templater);
-      app.use('/', router);
 
-      // actually we are testing this method, but we only know if it works by sending a request
+      router = express.Router();
+      app.use('/', router);
       crudController('book', router, bookServiceMock);
+      // actually we are testing this method, but we only know if it works by sending a request
       requester = request(app);
     });
 
-    describe('Generation of default methods', () => {
-      it('should respond an index method', (done) => {
-        requester
-        .get('/book')
-        .end((err, res) => {
-          if (err) done(err);
-          expect(res.body).to.be.an('object');
-          expect(res.body).to.have.property('rows');
-          expect(res.body).to.have.property('count');
-          expect(res.body).to.not.have.property('wasTransactional');
-          expect(res.status).to.be.equals(200);
-          done();
-        });
+    it('should respond an index method', (done) => {
+      requester
+      .get('/book')
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('rows');
+        expect(res.body).to.have.property('count');
+        expect(res.body).to.not.have.property('wasTransactional');
+        expect(res.status).to.be.equals(200);
+        done();
       });
+    });
 
-      it('should respond a get method', (done) => {
-        requester
-        .get('/book/1')
-        .end((err, res) => {
-          if (err) done(err);
-          expect(res.body).to.be.an('object');
-          expect(res.body).to.have.property('title', 'my little pony');
-          expect(res.body).to.not.have.property('wasTransactional');
-          expect(res.status).to.be.equals(200);
-          done();
-        });
+    it('should respond a get method', (done) => {
+      requester
+      .get('/book/1')
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('title', 'my little pony');
+        expect(res.body).to.not.have.property('wasTransactional');
+        expect(res.status).to.be.equals(200);
+        done();
       });
+    });
 
-      it('should respond a post method', (done) => {
-        requester
-        .post('/book')
-        .end((err, res) => {
-          if (err) done(err);
-          expect(res.body).to.be.an('object');
-          expect(res.body).to.have.property('title', 'my created little pony');
-          expect(res.body).to.have.property('wasTransactional', true);
-          expect(res.status).to.be.equals(201);
-          done();
-        });
+    it('should respond a post method', (done) => {
+      requester
+      .post('/book')
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('title', 'my created little pony');
+        expect(res.body).to.have.property('wasTransactional', true);
+        expect(res.status).to.be.equals(201);
+        done();
       });
+    });
 
-      it('should respond a put method', (done) => {
-        requester
-        .put('/book/1')
-        .end((err, res) => {
-          if (err) done(err);
-          expect(res.body).to.be.an('object');
-          expect(res.body).to.have.property('title', 'my updated little pony');
-          expect(res.body).to.have.property('wasTransactional', true);
-          expect(res.status).to.be.equals(200);
-          done();
-        });
+    it('should respond a put method', (done) => {
+      requester
+      .put('/book/1')
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('title', 'my updated little pony');
+        expect(res.body).to.have.property('wasTransactional', true);
+        expect(res.status).to.be.equals(200);
+        done();
       });
+    });
 
-      it('should respond a delete method', (done) => {
-        requester
-        .delete('/book/1')
-        .end((err, res) => {
-          if (err) done(err);
-          expect(res.body).to.be.an('object');
-          expect(res.body).to.have.property('title', 'my deleted little pony');
-          expect(res.body).to.have.property('wasTransactional', true);
-          expect(res.status).to.be.equals(200);
-          done();
-        });
+    it('should respond a delete method', (done) => {
+      requester
+      .delete('/book/1')
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('title', 'my deleted little pony');
+        expect(res.body).to.have.property('wasTransactional', true);
+        expect(res.status).to.be.equals(200);
+        done();
+      });
+    });
+  });
+
+  describe('Custom configurations', () => {
+    let app;
+    let router;
+
+    // this time we reset the app and the router everytime
+    beforeEach(() => {
+      app = express();
+      app.use(templater);
+
+      router = express.Router();
+      app.use('/', router);
+    });
+
+    it('should respond responde normally to a get petition (index)', (done) => {
+      crudController('book', router, bookServiceMock, ['index']);
+
+      request(app)
+      .get('/book')
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res.status).to.be.equals(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('rows');
+        expect(res.body).to.have.property('count');
+        done();
+      });
+    });
+
+    it('should not respond to a post method', (done) => {
+      crudController('book', router, bookServiceMock, ['index']);
+
+      request(app)
+      .post('/book')
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res.status).to.be.equals(404);
+        done();
+      });
+    });
+
+    it('should respond an index method transactionally', (done) => {
+      crudController('book', router, bookServiceMock, [
+        { method: 'index', transactional: true },
+      ]);
+
+      request(app)
+      .get('/book')
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res.status).to.be.equals(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('rows');
+        expect(res.body).to.have.property('count');
+        expect(res.body).to.have.property('wasTransactional', true);
+        done();
+      });
+    });
+
+    it('should respond an post method non transactionally', (done) => {
+      crudController('book', router, bookServiceMock, [
+        { method: 'post', transactional: false },
+      ]);
+
+      request(app)
+      .post('/book')
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res.status).to.be.equals(201);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('title', 'my created little pony');
+        expect(res.body).to.not.have.property('wasTransactional');
+        done();
       });
     });
   });
