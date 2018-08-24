6a8aad891106a3c239a4a7fb8fefc38667c9a8df
nardhar <nardhar.mure@gmail.com>
2018-08-24 18:27:12 -0400

    extracted middleware loading and cleaning up code

--- loaders/controller.js
+++ loaders/controller.js
@@ -2,85 +2,38 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const templater = require('../util/templater');
 const { loaddirSync } = require('../util/file');
 const serviceLoader = require('./service');
+const middlewareLoader = require('./middleware');
 
 module.exports = (config, models) => {
-  const services = serviceLoader(config, models);
-
+  // creating the express app
   const app = express();
 
-  app.use(cors({
+  // loading config.cors or the default cors configuration
+  app.use(cors(config.cors || {
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: true,
     headers: 'Cache-Control, Pragma, Content-Type, Authorization, Content-Length, X-Requested-With',
     'Access-Control-Allow-Headers': 'Authorization, Content-Type',
-    // 'Cache-Control': 'private, no-cache, no-store, must-revalidate',
-    // 'Expires': '-1',
-    // 'Pragma': 'no-cache',
   }));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
 
-  // loads our templater
-  templater.options({
-    template: (object, defaultData, responseArgs, req) => { // eslint-disable-line no-unused-vars
-      return object;
-    },
-  });
-  app.use(templater);
+  // loading config.globalMiddleware or the default global middlewares
+  (config.globalMiddleware || [
+    bodyParser.json(),
+    bodyParser.urlencoded({ extended: false }),
+  ])
+  .forEach(app.use);
+
+  // loading the services since we well pass them to the middlewares and the controllers
+  const services = serviceLoader(config, models);
 
   // creates the express router
   const router = express.Router();
 
-  // requires the app middlewares
-  const configMiddleware = config.middleware || {};
-  loaddirSync(
-    path.resolve(config.appPath, 'dir' in configMiddleware ? configMiddleware.dir : 'middlewares'),
-    `${'suffix' in configMiddleware ? configMiddleware.suffix : '.middleware'}.js`,
-    configMiddleware.ignore || [],
-  )
-  .map((middlewareFile) => {
-    return require(middlewareFile.path.substr(0, middlewareFile.path.lastIndexOf('.')))(services);
-  })
-  // adds the OPTIONS middleware
-  .concat({
-    order: 0,
-    callback: (req, res, next) => {
-      // it will jump all the middlewares in the same path if method === 'OPTIONS'
-      if (req.method === 'OPTIONS') {
-        next('route');
-      } else {
-        next();
-      }
-    },
-  })
-  // it orders the middlewares and loads them in the router
-  .sort((a, b) => {
-    return (a.order || 100) - (b.order || 100);
-  })
-  .reduce((middlewareGroupList, middleware) => {
-    // if no path is defined then it uses the general path: '*'
-    const middlewarePath = middleware.path || '*';
-    const idx = middlewareGroupList.findIndexOf((group) => {
-      return group.path === middlewarePath;
-    });
-
-    if (idx < 0) {
-      middlewareGroupList.push({
-        path: middlewarePath,
-        middlewareList: [middleware],
-      });
-      return middlewareGroupList;
-    }
-
-    middlewareGroupList[idx].middlewareList.push(middleware);
-
-    return middlewareGroupList;
-  }, [])
-  .forEach((middlewareGroupList) => {
+  // it imports all the middlewares in the router within its path
+  middlewareLoader(config, services).forEach((middlewareGroupList) => {
     router.use(middlewareGroupList.path, middlewareGroupList.middlewareList);
   });
 
---
+++ loaders/middleware.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const { loaddirSync } = require('../util/file');
+
+module.exports = (config, services) => {
+  // requires the app middlewares
+  const configMiddleware = config.middleware || {};
+  // loads the middlewares from the configured folder
+  return loaddirSync(
+    path.resolve(config.appPath, 'dir' in configMiddleware ? configMiddleware.dir : 'middlewares'),
+    `${'suffix' in configMiddleware ? configMiddleware.suffix : '.middleware'}.js`,
+    configMiddleware.ignore || [],
+  )
+  // requires each middleware
+  .map((middlewareFile) => {
+    return require(middlewareFile.path.substr(0, middlewareFile.path.lastIndexOf('.')))(services);
+  })
+  // adds the OPTIONS middleware
+  .concat({
+    // its path is = '*' so it will run in every route
+    path: '*',
+    // with order = 0 so it will run sooner
+    // (no other middlewares should have order < 0, unless you know what you're doing)
+    order: 0,
+    // and its corresponding callback
+    callback(req, res, next) {
+      // it will jump all the middlewares in the same path if method === 'OPTIONS'
+      if (req.method === 'OPTIONS') {
+        next('route');
+      } else {
+        next();
+      }
+    },
+  })
+  // it sorts the middlewares and loads them in the router
+  .sort((a, b) => {
+    // if a middleware does not have an order then it is = 100
+    return (a.order || 100) - (b.order || 100);
+  })
+  // groups the middlewares by path taking its order in account
+  .reduce((middlewareGroupList, middleware) => {
+    // if no path is defined then it uses the general path: '*'
+    const middlewarePath = middleware.path || '*';
+    // searchs its corresponding position in the resulting array
+    const idx = middlewareGroupList.findIndexOf((group) => {
+      // it searches by path
+      return group.path === middlewarePath;
+    });
+
+    // if the group is not found then adds a new item
+    if (idx < 0) {
+      middlewareGroupList.push({ path: middlewarePath, middlewareList: [middleware] });
+    } else {
+      // if it is found then adds it to the middleware list
+      middlewareGroupList[idx].middlewareList.push(middleware);
+    }
+
+    // and returns the result
+    return middlewareGroupList;
+  }, []);
+};
