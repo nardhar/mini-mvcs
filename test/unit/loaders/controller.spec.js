b7c82058145a51c76f0df7eefd5f3de16ee7de1e
nardhar <nardhar.mure@gmail.com>
2018-09-04 19:28:38 -0400

    starting controller tests

--- test/unit/loaders/controller.spec.js
+++ test/unit/loaders/controller.spec.js
@@ -1,5 +1,34 @@
+const path = require('path');
 const { expect } = require('chai');
 const rewiremock = require('rewiremock').default;
+
+rewiremock('path').with({
+  resolve(...args) {
+    return args.join('/');
+  },
+  sep: path.sep,
+});
+rewiremock('../util/file').with({
+  loaddirSync(folder, suffix, ignore) {
+    return [
+      'book.controller.js',
+      'author.controller.js',
+      'ignore.controller.js',
+      'notacontroller.js',
+    ]
+    .map((file) => {
+      return { file, path: `${folder}${path.sep}${file}` };
+    })
+    .filter((file) => {
+      return file.file.slice(-suffix.length) === suffix && ignore.indexOf(file.file) < 0;
+    });
+  },
+  normalizeName(name) {
+    return name.split(/-|_/).reduce((acc, val) => {
+      return !acc ? val : `${acc}${val.charAt(0).toUpperCase()}${val.slice(1)}`;
+    }, '');
+  },
+});
 
 let controllerLoader;
 
