22873e78886787432b7fb9ae185de872936f4964
nardhar <nardhar.mure@gmail.com>
2018-11-19 15:50:13 -0400

    controller unit tests

--- src/loaders/controller.js
+++ src/loaders/controller.js
@@ -1,48 +1,7 @@
 const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const { loaddirSync } = require('../util/file');
-const serviceLoader = require('./service');
-const middlewareLoader = require('./middleware');
-const templater = require('../util/templater');
-const errorHandleLoader = require('./error-handler');
 
-module.exports = (config, models) => {
-  // creating the express app
-  const app = express();
-
-  // loading config.cors or the default cors configuration
-  app.use(cors(config.cors || {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: true,
-    headers: 'Cache-Control, Pragma, Content-Type, Authorization, Content-Length, X-Requested-With',
-    'Access-Control-Allow-Headers': 'Authorization, Content-Type',
-  }));
-
-  // loading config.globalMiddleware or the default global middlewares
-  (config.globalMiddleware || [
-    bodyParser.json(),
-    bodyParser.urlencoded({ extended: false }),
-  ])
-  .forEach(app.use);
-
-  // loading the services since we well pass them to the middlewares and the controllers
-  const services = serviceLoader(config, models);
-
-  // creates the express router
-  const router = express.Router();
-
-  // it imports all the middlewares in the router within its path
-  middlewareLoader(config, services).forEach((middlewareGroupList) => {
-    router.use(middlewareGroupList.path, middlewareGroupList.middlewareList);
-  });
-
-  // imports the templater
-  const templateRouter = templater(config, router);
-
-  // it loads the controllers
+module.exports = (config, router, services) => {
   const configController = config.controller || {};
   loaddirSync(
     path.resolve(config.appPath, 'dir' in configController ? configController.dir : 'controllers'),
@@ -50,16 +9,6 @@ module.exports = (config, models) => {
     configController.ignore || [],
   )
   .forEach((file) => {
-    // NOTE: refactor a router wrapper for templating the response with status and a custom format
-    require(file.path.substr(0, file.path.lastIndexOf('.')))(templateRouter, services);
+    require(file.path.substr(0, file.path.lastIndexOf('.')))(router, services);
   });
-
-  // it loads the router into the app
-  const configApi = config.api || {};
-  app.use(configApi.main || '/api/v1', router);
-
-  // it loads the generated error handler
-  app.use(errorHandleLoader(config));
-
-  return app;
 };
--- test/unit/loaders/controller.spec.js
+++ test/unit/loaders/controller.spec.js
@@ -1,13 +1,15 @@
-const path = require('path');
+const { sep } = require('path');
 const { expect } = require('chai');
 const rewiremock = require('rewiremock').default;
+const Router = require('../../mocks/router.mock');
 
 rewiremock('path').with({
   resolve(...args) {
     return args.join('/');
   },
-  sep: path.sep,
+  sep,
 });
+
 rewiremock('../util/file').with({
   loaddirSync(folder, suffix, ignore) {
     return [
@@ -17,7 +19,7 @@ rewiremock('../util/file').with({
       'notacontroller.js',
     ]
     .map((file) => {
-      return { file, path: `${folder}${path.sep}${file}` };
+      return { file, path: `${folder}${sep}${file}` };
     })
     .filter((file) => {
       return file.file.slice(-suffix.length) === suffix && ignore.indexOf(file.file) < 0;
@@ -30,25 +32,81 @@ rewiremock('../util/file').with({
   },
 });
 
-const expressMock = () => {
-  return {
-    use(...args) {
-
+const serviceMock = {
+  book: {
+    save() {
+      return Promise.resolve({ title: 'sample' });
     },
-  };
-};
-expressMock.Router = () => {
-  return {
-    use(...args) {
-
+    list() {
+      return Promise.resolve([{ title: 'sample' }]);
     },
-  };
+  },
 };
-rewiremock('express').with(expressMock);
+
+rewiremock('../../test/resource/srcSample/controllers/book.controller').with((router, services) => {
+  router.get('/book', (req, res, next) => {
+    return services.book.list()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch(next);
+  });
+
+  router.get('/book/:id', (req, res, next) => {
+    return services.book.list()
+    .then((bookList) => {
+      return bookList[0];
+    })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch(next);
+  });
+
+  router.post('/book', (req, res, next) => {
+    return services.book.save()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch(next);
+  });
+
+  router.put('/book/:id', (req, res, next) => {
+    return services.book.save()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch(next);
+  });
+});
+
+rewiremock('../../test/resource/srcSample/controllers/author.controller').with((router) => {
+  router.get('/author', (req, res, next) => {
+    return Promise.resolve({ author: { name: 'john' } })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch(next);
+  });
+});
+
+rewiremock('../../test/resource/srcSample/controllers/ignore.controller').with((router) => {
+  router.get('/ignore', (req, res, next) => {
+    return Promise.resolve({ sample: { no: 'john' } })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch(next);
+  });
+});
+
+let routerMock;
+let result;
+let resMock;
 
 let controllerLoader;
 
-describe('Unit Testing CRUD Controller', () => {
+describe('Unit Testing Controller Loader', () => {
   before(() => {
     rewiremock.enable();
     controllerLoader = require('../../../src/loaders/controller');
@@ -56,7 +114,63 @@ describe('Unit Testing CRUD Controller', () => {
 
   after(() => { rewiremock.disable(); });
 
-  describe('Loading of default controllers folder', () => {
+  beforeEach(() => {
+    routerMock = new Router();
+    result = {};
+    resMock = {
+      headersSent: false,
+      status(code) {
+        result.code = code;
+        return {
+          json(data) {
+            result.data = data;
+          },
+        };
+      },
+    };
+  });
 
+  describe('Loading of default controllers folder', () => {
+    it('should execute a simple get action from a loaded controller', (done) => {
+      controllerLoader(
+        {
+          // since rewiremock only mocks existing modules we need to create the same file structure
+          // for importing the dynamically called services with require, even though they will be
+          // mocked here. Note: appPath must be relative to src/loaders/controller
+          appPath: '../../test/resource/srcSample',
+        },
+        routerMock,
+        serviceMock,
+      );
+      // the app should respond to the loaded controller paths
+      routerMock.execute({ originalUrl: '/book', method: 'get' }, resMock)
+      .then(() => {
+        expect(result).to.have.property('data');
+        expect(result.data).to.be.a('array');
+        done();
+      })
+      .catch(done);
+    });
+
+    it('should execute a save action from a loaded controller', (done) => {
+      controllerLoader(
+        {
+          // since rewiremock only mocks existing modules we need to create the same file structure
+          // for importing the dynamically called services with require, even though they will be
+          // mocked here. Note: appPath must be relative to src/loaders/controller
+          appPath: '../../test/resource/srcSample',
+        },
+        routerMock,
+        serviceMock,
+      );
+      // the app should respond to the loaded controller paths
+      routerMock.execute({ originalUrl: '/book', method: 'post' }, resMock)
+      .then(() => {
+        expect(result).to.have.property('data');
+        expect(result.data).to.have.property('title', 'sample');
+        done();
+      })
+      .catch(done);
+    });
   });
 });
